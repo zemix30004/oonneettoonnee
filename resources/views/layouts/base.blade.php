a26e7b3f6b5ad9910ecaf1b55530aad030389399
zemix30004 <eugenezm@gmail.com>
2022-01-31 15:58:42 +0200

    added base shop admin panel ccheckout

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -42,8 +42,7 @@
 						</div>
 						<div class="topbar-menu right-menu">
 							<ul>
-								<li class="menu-item" ><a title="Register or Login" href="login.html">Login</a></li>
-								<li class="menu-item" ><a title="Register or Login" href="register.html">Register</a></li>
+
 								<li class="menu-item lang-menu menu-item-has-children parent">
 									<a title="English" href="#"><span class="img label-before"><img src="assets/images/lang-en.png" alt="lang-en"></span>English<i class="fa fa-angle-down" aria-hidden="true"></i></a>
 									<ul class="submenu lang" >
@@ -53,20 +52,35 @@
 										<li class="menu-item" ><a title="canada" href="#"><span class="img label-before"><img src="assets/images/lang-can.png" alt="lang-can"></span>Canada</a></li>
 									</ul>
 								</li>
-								<li class="menu-item menu-item-has-children parent" >
-									<a title="Dollar (USD)" href="#">Dollar (USD)<i class="fa fa-angle-down" aria-hidden="true"></i></a>
-									<ul class="submenu curency" >
-										<li class="menu-item" >
-											<a title="Pound (GBP)" href="#">Pound (GBP)</a>
-										</li>
-										<li class="menu-item" >
-											<a title="Euro (EUR)" href="#">Euro (EUR)</a>
-										</li>
-										<li class="menu-item" >
-											<a title="Dollar (USD)" href="#">Dollar (USD)</a>
-										</li>
-									</ul>
-								</li>
+
+                                @if(Route::has('login'))
+                                    @auth
+                                        @if(Auth::user()->utype() === 'ADM')
+                                        //Admin
+                                            <li class="menu-item menu-item-has-children parent" >
+									            <a title="My Account" href="#">My Account ({{ Auth::user->name }})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+									            <ul class="submenu curency" >
+                                                    <li class="menu-item" >
+                                                        <a title="Dashboard" href="#">Dashboard</a>
+                                                    </li>
+									            </ul>
+								            </li>
+                                        @else
+                                        //Guest
+                                            <li class="menu-item menu-item-has-children parent" >
+                                                <a title="My Account" href="#">My Account ({{ Auth::user->name }})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                                                <ul class="submenu curency" >
+                                                    <li class="menu-item" >
+                                                        <a title="Dashboard" href="#">Dashboard</a>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                        @endif
+                                    @else
+                                        <li class="menu-item" ><a title="Register or Login" href="{{ route('login') }}">Login</a></li>
+                                        <li class="menu-item" ><a title="Register or Login" href="{{ route('register') }}">Register</a></li>
+                                    @endif
+                                @endif
 							</ul>
 						</div>
 					</div>
@@ -159,19 +173,19 @@
 						<div class="container">
 							<ul class="nav primary clone-main-menu" id="mercado_main" data-menuname="Main menu" >
 								<li class="menu-item home-icon">
-									<a href="index.html" class="link-term mercado-item-title"><i class="fa fa-home" aria-hidden="true"></i></a>
+									<a href="/" class="link-term mercado-item-title"><i class="fa fa-home" aria-hidden="true"></i></a>
 								</li>
 								<li class="menu-item">
 									<a href="about-us.html" class="link-term mercado-item-title">About Us</a>
 								</li>
 								<li class="menu-item">
-									<a href="shop.html" class="link-term mercado-item-title">Shop</a>
+									<a href="/shop" class="link-term mercado-item-title">Shop</a>
 								</li>
 								<li class="menu-item">
-									<a href="cart.html" class="link-term mercado-item-title">Cart</a>
+									<a href="/cart" class="link-term mercado-item-title">Cart</a>
 								</li>
 								<li class="menu-item">
-									<a href="checkout.html" class="link-term mercado-item-title">Checkout</a>
+									<a href="/checkout" class="link-term mercado-item-title">Checkout</a>
 								</li>
 								<li class="menu-item">
 									<a href="contact-us.html" class="link-term mercado-item-title">Contact Us</a>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +22,10 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', HomeComponent::class);
+Route::get('/shop', ShopComponent::class);
+Route::get('/cart', CartComponent::class);
+Route::get('/checkout', CheckoutComponent::class);
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
